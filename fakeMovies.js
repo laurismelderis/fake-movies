be1e165d8cef9e9846cac2e4f64218d3185fc699
LaurisM <lauris.melderis@accenture.com>
2022-08-03 11:49:16 +0300

    Add fake movies

---
+++ fakeMovies.js
@@ -0,0 +1,62 @@
+module.exports = [
+    {
+        name: 'Batman',
+        genre: 'Action',
+        rentalPrice: 4.55,
+        countInStock: 5
+    },
+    {
+        name: 'Notebook',
+        genre: 'Romantic',
+        rentalPrice: 3.45,
+        countInStock: 0
+    },
+    {
+        name: 'Idiana Jones',
+        genre: 'Adventure',
+        rentalPrice: 2.99,
+        countInStock: 0
+    },
+    {
+        name: 'Matrix',
+        genre: 'Action',
+        rentalPrice: 5.10,
+        countInStock: 4
+    },
+    {
+        name: 'Vikings',
+        genre: 'Action',
+        rentalPrice: 3.34,
+        countInStock: 2
+    },
+    {
+        name: 'Interstellar',
+        genre: 'Sci-fi',
+        rentalPrice: 9.45,
+        countInStock: 1
+    },
+    {
+        name: 'The Hobbit',
+        genre: 'Adventure',
+        rentalPrice: 6.30,
+        countInStock: 8
+    },
+    {
+        name: "Le Mans '66",
+        genre: 'Drama',
+        rentalPrice: 11.99,
+        countInStock: 2
+    },
+    {
+        name: 'Cast Away',
+        genre: 'Drama',
+        rentalPrice: 7.49,
+        countInStock: 5
+    },
+    {
+        name: 'The Lost City',
+        genre: 'Romantic',
+        rentalPrice: 7.49,
+        countInStock: 5
+    },
+]
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello world')
+const express = require('express')
+const cors = require('cors')
+const movies = require('./fakeMovies')
+const app = express()
+
+const PORT = 3000 || 3900
+
+app.use(
+    cors({
+        origin: `http://localhost:${PORT}`
+    })
+)
+
+app.get('/api/movies', (req, res) => {
+    res.send(movies)
+})
+
+app.listen(PORT, 'localhost', () => {
+    console.info(`Listening on port ${PORT} ...`)
+})
